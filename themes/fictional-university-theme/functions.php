ba637bd518248fc45cda62cdf0aed58b84591abf
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-05-15 23:52:48 +0530

    done with assignment

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -88,6 +88,7 @@ function university_features()
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
   add_image_size('pageBanner', 1500, 350, true);
+  add_image_size('slideshowImage', 1900, 525, true);
 }
 
 add_action('after_setup_theme', 'university_features');
